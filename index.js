a0bcc18a5f74cb85735aac7617df5c41181ef529
Liam Killion <liam.killion@gmail.com>
2017-12-05 21:29:02 -0500

    Game modal, image flips, sidebar

---
+++ index.js
@@ -0,0 +1,11 @@
+//~~~~~~~~~~~~~~~~~~ ALL CARD FLIP ANI~~~~~~~~~~~~~~~~~~
+$(".card").flip()
+//~~~~~~~~~~~~~~~~~~ ALL SIDE NAV ANI~~~~~~~~~~~~~~~~~~
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+//~~~~~~~~~~~~~~~~~~ ALL SIDE NAV ANI~~~~~~~~~~~~~~~~~~
